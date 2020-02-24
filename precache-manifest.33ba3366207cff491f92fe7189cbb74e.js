14129fcb58e6f7829d5abb2454adc06c4b070dde
jimchou223 <ethan790223@gmail.com>
2020-02-24 00:05:04 -0800

    Updates

--- precache-manifest.33ba3366207cff491f92fe7189cbb74e.js
+++ precache-manifest.33ba3366207cff491f92fe7189cbb74e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8aacdace413958eee2b4e660f5804211",
+    "revision": "aa093b592a643b19652aee5356daa7aa",
     "url": "/imenu-admin/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/imenu-admin/static/css/2.e3d692b7.chunk.css"
   },
   {
-    "revision": "b5b514abaa8177da6cf2",
+    "revision": "9acb1b100dccf88a7851",
     "url": "/imenu-admin/static/css/main.710cea22.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/imenu-admin/static/js/2.ce73741e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b5b514abaa8177da6cf2",
-    "url": "/imenu-admin/static/js/main.9e8b9d5e.chunk.js"
+    "revision": "9acb1b100dccf88a7851",
+    "url": "/imenu-admin/static/js/main.c9f4b47d.chunk.js"
   },
   {
     "revision": "4e32c38a1e7199e34603",
